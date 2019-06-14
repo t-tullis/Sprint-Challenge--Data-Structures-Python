9521037e9fb697fd75849c5b96ccd53c84790d8f
t-tullis <t.tullis34@gmail.com>
2019-06-14 13:47:17 -0700

    fixed bstree typos

--- names/names.py
+++ names/names.py
@@ -16,13 +16,13 @@ bstree = BinarySearchTree(names_1[0])
 #range from 1 - 10000
 for i in range(1, len(names_1)):
     #running bstree on names_1 txt file
-    bstree.insert(name_1[i])
+    bstree.insert(names_1[i])
 
 duplicates = []
 #loop through names_2 txt file
 for name in names_2:
     #if bstree from names_1 contains the name in names_2
-    if bst.contains(name):
+    if bstree.contains(name):
         #append the duplicate name to the duplicates array
         duplicates.append(name)
 
